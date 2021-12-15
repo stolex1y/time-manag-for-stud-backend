9f58f41bf1256f8b59cc5e8ec6d1e816b153b35d
Alexey Filimonov <stolexiy@mail.ru>
2021-12-15 18:38:26 +0300

    Import spring initializr project

---
+++ src/main/kotlin/timemanagement/TimeManagementApplication.kt
@@ -0,0 +1,11 @@
+package timemanagement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TimeManagementApplication
+
+fun main(args: Array<String>) {
+	runApplication<TimeManagementApplication>(*args)
+}
---
+++ src/test/kotlin/timemanagement/TimeManagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package timemanagement
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TimeManagementApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
